097aa2f4fde113edfe844f24ccf405e53800708b
DianCh <namedcd@gmail.com>
2021-03-22 17:23:53 -0700

    added masking utlities in dataset_mapper

--- detectron2/data/dataset_mapper.py
+++ detectron2/data/dataset_mapper.py
@@ -1,11 +1,13 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 import copy
+import random
 import logging
 import numpy as np
 from typing import List, Optional, Union
 import torch
 
 from detectron2.config import configurable
+from detectron2.structures import BoxMode
 
 from . import detection_utils as utils
 from . import transforms as T
@@ -47,6 +49,9 @@ class DatasetMapper:
         keypoint_hflip_indices: Optional[np.ndarray] = None,
         precomputed_proposal_topk: Optional[int] = None,
         recompute_boxes: bool = False,
+        mask_margin: int = 32,
+        keep_ratio: Optional[float] = None,
+        region_box_count: Optional[int] = None,
     ):
         """
         NOTE: this interface is experimental.
@@ -77,19 +82,36 @@ class DatasetMapper:
         self.keypoint_hflip_indices = keypoint_hflip_indices
         self.proposal_topk          = precomputed_proposal_topk
         self.recompute_boxes        = recompute_boxes
+        self.mask_margin            = mask_margin
+        self.keep_ratio             = keep_ratio
+        self.region_box_count       = region_box_count
         # fmt: on
         logger = logging.getLogger(__name__)
         mode = "training" if is_train else "inference"
-        logger.info(f"[DatasetMapper] Augmentations used in {mode}: {augmentations}")
+        logger.info(
+            f"[DatasetMapper] Augmentations used in {mode}: {augmentations}"
+        )
 
     @classmethod
     def from_config(cls, cfg, is_train: bool = True):
         augs = utils.build_augmentation(cfg, is_train)
         if cfg.INPUT.CROP.ENABLED and is_train:
-            augs.insert(0, T.RandomCrop(cfg.INPUT.CROP.TYPE, cfg.INPUT.CROP.SIZE))
+            augs.insert(
+                0, T.RandomCrop(cfg.INPUT.CROP.TYPE, cfg.INPUT.CROP.SIZE)
+            )
             recompute_boxes = cfg.MODEL.MASK_ON
         else:
             recompute_boxes = False
+
+        mask_margin = (
+            cfg.INPUT.MASK_MARGIN if "MASK_MARGIN" in cfg.INPUT else 32
+        )
+        keep_ratio = cfg.INPUT.KEEP_RATIO if "KEEP_RATIO" in cfg.INPUT else None
+        region_box_count = (
+            cfg.INPUT.REGION_BOX_COUNT
+            if "REGION_BOX_COUNT" in cfg.INPUT
+            else None
+        )
 
         ret = {
             "is_train": is_train,
@@ -99,10 +121,15 @@ class DatasetMapper:
             "instance_mask_format": cfg.INPUT.MASK_FORMAT,
             "use_keypoint": cfg.MODEL.KEYPOINT_ON,
             "recompute_boxes": recompute_boxes,
+            "mask_margin": mask_margin,
+            "keep_ratio": keep_ratio,
+            "region_box_count": region_box_count,
         }
 
         if cfg.MODEL.KEYPOINT_ON:
-            ret["keypoint_hflip_indices"] = utils.create_keypoint_hflip_indices(cfg.DATASETS.TRAIN)
+            ret["keypoint_hflip_indices"] = utils.create_keypoint_hflip_indices(
+                cfg.DATASETS.TRAIN
+            )
 
         if cfg.MODEL.LOAD_PROPOSALS:
             ret["precomputed_proposal_topk"] = (
@@ -120,14 +147,20 @@ class DatasetMapper:
         Returns:
             dict: a format that builtin models in detectron2 accept
         """
-        dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
+        dataset_dict = copy.deepcopy(
+            dataset_dict
+        )  # it will be modified by code below
         # USER: Write your own image loading if it's not from a file
-        image = utils.read_image(dataset_dict["file_name"], format=self.image_format)
+        image = utils.read_image(
+            dataset_dict["file_name"], format=self.image_format
+        )
         utils.check_image_size(dataset_dict, image)
 
         # USER: Remove if you don't do semantic/panoptic segmentation.
         if "sem_seg_file_name" in dataset_dict:
-            sem_seg_gt = utils.read_image(dataset_dict.pop("sem_seg_file_name"), "L").squeeze(2)
+            sem_seg_gt = utils.read_image(
+                dataset_dict.pop("sem_seg_file_name"), "L"
+            ).squeeze(2)
         else:
             sem_seg_gt = None
 
@@ -139,7 +172,9 @@ class DatasetMapper:
         # Pytorch's dataloader is efficient on torch.Tensor due to shared-memory,
         # but not efficient on large generic data structures due to the use of pickle & mp.Queue.
         # Therefore it's important to use torch.Tensor.
-        dataset_dict["image"] = torch.as_tensor(np.ascontiguousarray(image.transpose(2, 0, 1)))
+        dataset_dict["image"] = torch.as_tensor(
+            np.ascontiguousarray(image.transpose(2, 0, 1))
+        )
         if sem_seg_gt is not None:
             dataset_dict["sem_seg"] = torch.as_tensor(sem_seg_gt.astype("long"))
 
@@ -147,7 +182,10 @@ class DatasetMapper:
         # Most users would not need this feature.
         if self.proposal_topk is not None:
             utils.transform_proposals(
-                dataset_dict, image_shape, transforms, proposal_topk=self.proposal_topk
+                dataset_dict,
+                image_shape,
+                transforms,
+                proposal_topk=self.proposal_topk,
             )
 
         if not self.is_train:
@@ -167,7 +205,10 @@ class DatasetMapper:
             # USER: Implement additional transformations if you have other types of data
             annos = [
                 utils.transform_instance_annotations(
-                    obj, transforms, image_shape, keypoint_hflip_indices=self.keypoint_hflip_indices
+                    obj,
+                    transforms,
+                    image_shape,
+                    keypoint_hflip_indices=self.keypoint_hflip_indices,
                 )
                 for obj in dataset_dict.pop("annotations")
                 if obj.get("iscrowd", 0) == 0
@@ -185,3 +226,172 @@ class DatasetMapper:
                 instances.gt_boxes = instances.gt_masks.get_bounding_boxes()
             dataset_dict["instances"] = utils.filter_empty_instances(instances)
         return dataset_dict
+
+    def black_out_image(self, image, regions):
+        """Keep specified regions while black out the other areas."""
+        _, h, w = image.shape
+        mask = torch.zeros((h, w), dtype=torch.bool)
+        for region in regions:
+            xmin, ymin, xmax, ymax = region
+            mask[xmin:xmax, ymin:ymax] = 1
+
+        return image * mask
+
+    def expand_margins(self, regions):
+        regions = [
+            (
+                max(0, int(box[0] - self.mask_margin)),
+                max(0, int(box[1] - self.mask_margin)),
+                int(box[2] + self.mask_margin),
+                int(box[3] + self.mask_margin),
+            )
+            for box in regions
+        ]
+        return regions
+
+
+class DatasetMapperInstanceMask(DatasetMapper):
+    def __call__(self, dataset_dict):
+        """
+        Args:
+            dataset_dict (dict): Metadata of one image, in Detectron2 Dataset format.
+
+        Returns:
+            dict: a format that builtin models in detectron2 accept
+        """
+        dataset_dict = copy.deepcopy(
+            dataset_dict
+        )  # it will be modified by code below
+        # USER: Write your own image loading if it's not from a file
+        image = utils.read_image(
+            dataset_dict["file_name"], format=self.image_format
+        )
+        utils.check_image_size(dataset_dict, image)
+
+        # USER: Remove if you don't do semantic/panoptic segmentation.
+        if "sem_seg_file_name" in dataset_dict:
+            sem_seg_gt = utils.read_image(
+                dataset_dict.pop("sem_seg_file_name"), "L"
+            ).squeeze(2)
+        else:
+            sem_seg_gt = None
+
+        aug_input = T.AugInput(image, sem_seg=sem_seg_gt)
+        transforms = self.augmentations(aug_input)
+        image, sem_seg_gt = aug_input.image, aug_input.sem_seg
+
+        image_shape = image.shape[:2]  # h, w
+        # Pytorch's dataloader is efficient on torch.Tensor due to shared-memory,
+        # but not efficient on large generic data structures due to the use of pickle & mp.Queue.
+        # Therefore it's important to use torch.Tensor.
+        dataset_dict["image"] = torch.as_tensor(
+            np.ascontiguousarray(image.transpose(2, 0, 1))
+        )
+        if sem_seg_gt is not None:
+            dataset_dict["sem_seg"] = torch.as_tensor(sem_seg_gt.astype("long"))
+
+        # USER: Remove if you don't use pre-computed proposals.
+        # Most users would not need this feature.
+        if self.proposal_topk is not None:
+            utils.transform_proposals(
+                dataset_dict,
+                image_shape,
+                transforms,
+                proposal_topk=self.proposal_topk,
+            )
+
+        if not self.is_train:
+            # USER: Modify this if you want to keep them for some reason.
+            dataset_dict.pop("annotations", None)
+            dataset_dict.pop("sem_seg_file_name", None)
+            return dataset_dict
+
+        if "annotations" in dataset_dict:
+            # USER: Modify this if you want to keep them for some reason.
+            for anno in dataset_dict["annotations"]:
+                if not self.use_instance_mask:
+                    anno.pop("segmentation", None)
+                if not self.use_keypoint:
+                    anno.pop("keypoints", None)
+
+            # USER: Implement additional transformations if you have other types of data
+            annos = [
+                utils.transform_instance_annotations(
+                    obj,
+                    transforms,
+                    image_shape,
+                    keypoint_hflip_indices=self.keypoint_hflip_indices,
+                )
+                for obj in dataset_dict.pop("annotations")
+                if obj.get("iscrowd", 0) == 0
+            ]
+            # vvv Added for black-out
+            annos = self.sample_annotations(annos)
+            regions = self.get_mask_regions(annos)
+            dataset_dict["image"] = self.black_out_image(
+                dataset_dict["image"], regions
+            )
+            # ^^^ Added for black-out
+            instances = utils.annotations_to_instances(
+                annos, image_shape, mask_format=self.instance_mask_format
+            )
+
+            # After transforms such as cropping are applied, the bounding box may no longer
+            # tightly bound the object. As an example, imagine a triangle object
+            # [(0,0), (2,0), (0,2)] cropped by a box [(1,0),(2,2)] (XYXY format). The tight
+            # bounding box of the cropped triangle should be [(1,0),(2,1)], which is not equal to
+            # the intersection of original bounding box and the cropping box.
+            if self.recompute_boxes:
+                instances.gt_boxes = instances.gt_masks.get_bounding_boxes()
+            dataset_dict["instances"] = utils.filter_empty_instances(instances)
+        return dataset_dict
+
+    def sample_annotations(self, annotations):
+        if not self.keep_ratio:
+            return annotations
+
+        num_keep = int(self.keep_ratio * len(annotations))
+        random.shuffle(annotations)
+
+        return annotations[:num_keep]
+
+    def get_mask_regions(self, annotations):
+        """In this naive version of black-out, simply keep per-instance regions."""
+        regions = [
+            anno["bbox"]
+            for anno in annotations
+            if anno["bbox_mode"] == BoxMode.XYXY_ABS
+        ]
+        regions = self.expand_margins(regions)
+        return regions
+
+
+class DatasetMapperRegionMask(DatasetMapperInstanceMask):
+    def sample_annotations(self, annotations):
+        # randomly select one box
+        anchor = random.choice(annotations)["bbox"]
+        xc = 0.5 * (anchor[0] + anchor[2])
+        yc = 0.5 * (anchor[1] + anchor[3])
+
+        # annotations come after the XYXY_ABS conversion
+        boxes = [anno["bbox"] for anno in annotations]
+        offsets = [
+            (0.5 * (xmin + xmax) - xc, 0.5 * (ymin + ymax) - yc)
+            for xmin, ymin, xmax, ymax in boxes
+        ]
+        dists = [np.linalg.norm(np.array(offset)) for offset in offsets]
+        dists_boxes = [(dist, anno) for dist, anno in zip(dists, annotations)]
+        dists_boxes.sort(key=lambda x: x[0])
+
+        return [anno for _, anno in dists_boxes[: self.region_box_count]]
+
+    def get_mask_regions(self, annotations):
+        """In this version of black-out, simulate the region-wise selection."""
+        xmin = min(anno["bbox"][0] for anno in annotations)
+        ymin = min(anno["bbox"][1] for anno in annotations)
+        xmax = max(anno["bbox"][2] for anno in annotations)
+        ymax = max(anno["bbox"][3] for anno in annotations)
+
+        regions = [[xmin, ymin, xmax, ymax]]
+        regions = self.expand_margins(regions)
+        return regions
